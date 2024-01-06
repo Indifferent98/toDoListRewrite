4ff2f243e38b1327f26edfa784047aef579dff68
indifferent98 <zhenyayarmokova@mail.ru>
2024-01-06 13:00:47 +0300

    add todolist jest tests

--- src/reducers/todolists-reducer.test.ts
+++ src/reducers/todolists-reducer.test.ts
@@ -1,5 +1,8 @@
 import { v1 } from 'uuid'
 import {
+	addToDoListAC,
+	changeToDoListFilterAC,
+	changeToDoListTitleAC,
 	removeToDoListAC,
 	todolistReducer,
 	todolistsType,
@@ -25,4 +28,80 @@ test('todolist should be removed', () => {
 	expect(result.length).toBe(1)
 	expect(result[0].id).toBe(todolistid1)
 	expect(result[1]).toBe(undefined)
+})
+
+test('todolist should be added', () => {
+	const todolistid1 = v1()
+	const todolistid2 = v1()
+	const initialState: todolistsType[] = [
+		{
+			id: todolistid1,
+			title: 'what to learn',
+			filter: 'all',
+		},
+		{
+			id: todolistid2,
+			title: 'what to read',
+			filter: 'all',
+		},
+	]
+
+	const result = todolistReducer(initialState, addToDoListAC('newList'))
+
+	expect(result.length).toBe(3)
+	expect(result[2].title).toBe('newList')
+	expect(result[0].title).toBe('what to learn')
+	expect(result[1].title).toBe('what to read')
+})
+
+test('todolist filter should be changed', () => {
+	const todolistid1 = v1()
+	const todolistid2 = v1()
+	const initialState: todolistsType[] = [
+		{
+			id: todolistid1,
+			title: 'what to learn',
+			filter: 'all',
+		},
+		{
+			id: todolistid2,
+			title: 'what to read',
+			filter: 'all',
+		},
+	]
+
+	const result = todolistReducer(
+		initialState,
+		changeToDoListFilterAC('completed', todolistid2)
+	)
+
+	expect(result.length).toBe(2)
+	expect(result[1].filter).toBe('completed')
+	expect(result[0].filter).toBe('all')
+})
+
+test('todolist title should be changed', () => {
+	const todolistid1 = v1()
+	const todolistid2 = v1()
+	const initialState: todolistsType[] = [
+		{
+			id: todolistid1,
+			title: 'what to learn',
+			filter: 'all',
+		},
+		{
+			id: todolistid2,
+			title: 'what to read',
+			filter: 'all',
+		},
+	]
+
+	const result = todolistReducer(
+		initialState,
+		changeToDoListTitleAC('nowItsNewTitle', todolistid2)
+	)
+
+	expect(result.length).toBe(2)
+	expect(result[1].title).toBe('nowItsNewTitle')
+	expect(result[0].title).toBe('what to learn')
 })
